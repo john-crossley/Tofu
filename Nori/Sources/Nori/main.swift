0a4190c7d4be5d833484edc6c14518657184b650
John Crossley <jonnysnip3r@gmail.com>
2018-04-07 11:13:04 +0100

    Initial Commit

---
+++ Nori/Sources/Nori/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Nori
+//
+//  Created by John Crossley on 07/04/2018.
+//  Copyright © 2018 John Crossley. All rights reserved.
+//
+
+import Foundation
+import NIO
+import NIOHTTP1
+import NIOOpenSSL
+
